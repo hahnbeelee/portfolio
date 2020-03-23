a8be30137f76a02c33435dd3abc5646121b50e4b
Hahnbee Lee <hl985@cornell.edu>
2020-03-23 14:18:27 -0400

    Updates

---
+++ precache-manifest.242462751025efe858709dec199d411c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10dc3148d3d1a07ecc970a6bee4dcf31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de871f40b401ac3b0de7",
+    "url": "/static/css/main.2ae42328.chunk.css"
+  },
+  {
+    "revision": "6463f8752b2a076c32e2",
+    "url": "/static/js/2.bfaeb864.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bfaeb864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de871f40b401ac3b0de7",
+    "url": "/static/js/main.0d01556e.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "78f4dd6523be885ba15b1b36794b3f34",
+    "url": "/static/media/headshot.78f4dd65.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7d2821e4f9802ee2a86d1aa57256661d.js"
+  "/precache-manifest.242462751025efe858709dec199d411c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
